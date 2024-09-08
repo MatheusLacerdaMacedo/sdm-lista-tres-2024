64204c22a96d84c9d343d1af82ea1a9bc7eba30f
Matheus Lacerda Macedo <143509735+MatheusLacerdaMacedo@users.noreply.github.com>
2024-09-08 15:32:28 -0300

    Create Colegios.cs

---
+++ Colegios.cs
@@ -0,0 +1,11 @@
+ public class School
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Endereco { get; set; }
+        public string? Cep { get; set; }
+        public int Alunos { get; set; }
+        public int Funcionarios { get; set; }
+        public string? Grau { get; set; }
+        public double Mensalidade { get; set; }
+    }
